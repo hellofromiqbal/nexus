1ac1080ccc63773bf64c3680805654b79b787714
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-23 11:05:42 +0700

    add spinner

--- src/app/components/Forms/Register/RegisterForm.jsx
+++ src/app/components/Forms/Register/RegisterForm.jsx
@@ -6,11 +6,14 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { registerFormSchema } from '@/helpers/zodSchema';
+import Spinner from '../../Spinner/Spinner';
 
 const RegisterForm = () => {
+  const [isValidating, setIsValidating] = useState(false);
   const { register, handleSubmit, reset, formState: { errors } } = useForm({ resolver: zodResolver(registerFormSchema) });
 
   const submittedData = async (data) => {
+    setIsValidating((prev) => !prev);
     try {
       const res = await fetch('/api/users/register', {
         cache: 'no-store',
@@ -28,6 +31,7 @@ const RegisterForm = () => {
     } catch (error) {
       console.log(error.message);
     };
+    setIsValidating((prev) => !prev);
   };
 
   return (
@@ -68,7 +72,10 @@ const RegisterForm = () => {
             {errors.confirmPassword ? <p className="text-sm">{errors.confirmPassword.message}</p> : ''}
           </div>
         </div>
-        <button className="py-2 bg-green-500 hover:bg-green-600 text-white rounded-full">Register</button>
+        <button
+          className={`py-2 bg-green-500 hover:bg-green-600 text-white rounded-full ${isValidating && 'bg-slate-500 hover:bg-slate-500'} flex justify-center items-center`}
+          disabled={isValidating}
+        >{isValidating ? <Spinner/> : 'Register'}</button>
       </form>
       <div className="flex justify-center">
         <p className="text-sm text-white">Already have an account? <Link href={'/'} className="text-green-500 hover:text-green-600 underline">Login</Link> here.</p>
