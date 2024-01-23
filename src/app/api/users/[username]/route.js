56e3cedacb2a8c91db762df2b41a0e0e4bafab15
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-23 14:07:22 +0700

    add get single user by username

---
+++ src/app/api/users/[username]/route.js
@@ -0,0 +1,26 @@
+import connectMongoDB from "@/libs/mongodb";
+import User from "@/models/userModel";
+import { NextResponse } from 'next/server';
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectMongoDB();
+    const document = await User.findOne({ username: params.username });
+    if(!document) {
+      return NextResponse.json({
+        success: false,
+        message: 'User not found.'
+      }, { status: 404 });
+    };
+    
+    return NextResponse.json({
+      success: true,
+      data: document
+    }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: 'Failed to get user info.'
+    }, { status: 500 });
+  };
+};
