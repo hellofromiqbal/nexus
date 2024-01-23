17d96b7e33f9bd8b0e6d7f2a8f145017ac221dbe
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-23 14:19:40 +0700

    adjust main profile data fetching

--- src/app/components/MainProfile/MainProfile.jsx
+++ src/app/components/MainProfile/MainProfile.jsx
@@ -1,12 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileDetails from '../ProfileDetails/ProfileDetails';
 import ProfileButtons from '../ProfileButtons/ProfileButtons';
 import PostList from '../PostList/PostList';
+import { useParams } from 'next/navigation';
 
 const MainProfile = () => {
+  const params = useParams();
+  const username = decodeURIComponent(params.username);
   const [showPosts, setShowPosts] = useState('posts');
+  useEffect(() => {
+    fetch(`/api/users/${username}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data.data))
+      .catch((err) => console.log(err.message));
+  }, []);
+  
   return (
     <>
       <div>
