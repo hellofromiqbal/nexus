904df8eb40c793ada356f25c047c8c0291f78629
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-24 21:26:11 +0700

    add api route to get visited post

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -3,7 +3,42 @@ import Post from "@/models/postModel";
 import User from "@/models/userModel";
 import { NextResponse } from 'next/server';
 
-export const POST = async (request, {params}) => {
+export const GET = async (request, { params }) => {
+  try {
+    await connectMongoDB();
+    const document = await Post.findById(params.id).populate('author').populate({
+      path: 'likes',
+      populate: ({
+        path: 'author',
+        method: 'User'
+      })
+    }).populate({
+      path: 'replies',
+      populate: ({
+        path: 'author',
+        method: 'User'
+      })
+    });
+    if(!document) {
+      return NextResponse.json({
+        success: false,
+        message: 'Post not found.'
+      }, { status: 404 });
+    };
+    
+    return NextResponse.json({
+      success: true,
+      data: document
+    }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message
+    }, { status: 500 });
+  };
+};
+
+export const POST = async (request, { params }) => {
   try {
     await connectMongoDB();
     const document = await Post.findById(params.id);
