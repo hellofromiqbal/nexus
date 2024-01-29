9fec52aed6edbb1806d2a3dd3bcb4640db2f4b66
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-29 13:59:03 +0700

    add comming soon pages (explore, notifications, messages, and friends)

---
+++ src/app/dashboard/(coming soon)/notifications/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BackButton from '@/app/components/Buttons/Back/BackButton';
+
+const NotificationsPage = () => {
+  return (
+    <div className='flex flex-col bg-gray-800 shadow-md rounded-md'>
+      <div className='p-4 flex'>
+        <BackButton/>
+      </div>
+      <div>
+        <div className='h-[200px] flex justify-center items-center border-t border-gray-700'>
+          <h1 className='text-white font-medium'>Coming soon!</h1>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default NotificationsPage;
