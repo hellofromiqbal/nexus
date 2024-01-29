1a5e8b38f28644684aab4e16e4b6275af965295b
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-29 21:25:59 +0700

    unhide profile button in dashboard head

--- src/app/components/DashboardHead/DashboardHead.jsx
+++ src/app/components/DashboardHead/DashboardHead.jsx
@@ -12,11 +12,11 @@ const DashboardHead = () => {
   console.log(currentUser);
   return (
     <div className='flex bg-gray-800 md:bg-transparent border-b md:border-none border-gray-700 md:shadow-none justify-between md:justify-center items-center py-2'>
-      {/* <div className='md:hidden'>
+      <div className='md:hidden'>
         <Link href={`/dashboard/profile/${currentUser?.username}`} className={`p-4 text-white font-medium flex items-center gap-4`}>
           <FaUserCircle className='w-[25px] h-[25px]'/>
         </Link>
-      </div> */}
+      </div>
       <div>
         <h1 className="text-4xl font-bold text-white"><span className="text-green-500">N</span>exus</h1>
       </div>
