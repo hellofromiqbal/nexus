2a871bf61f4e504a62dd09df01e67bf5732887bd
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-30 10:51:57 +0700

    test

--- src/app/api/users/me/route.js
+++ src/app/api/users/me/route.js
@@ -7,31 +7,85 @@ export const GET = async (request, response) => {
   try {
     const currentUserToken = await request.cookies.get('nexus');
     const currentUserInfo = jwt.verify(currentUserToken.value, process.env.JWT_SECRET_TOKEN)._doc;
-    const currentUser = await User.findById(currentUserInfo._id).populate({
+    const currentUser = await User.findById(currentUserInfo._id)
+    .populate({
       path: 'posts',
-      populate: ({
-        path: 'author',
-        model: 'User'
-      })
-    }).populate({
+      populate: [
+        {
+          path: 'author',
+          model: 'User'
+        },
+        {
+          path: 'likes',
+          populate: {
+            path: 'author',
+            model: 'User'
+          }
+        },
+        {
+          path: 'replies',
+          model: 'Reply'
+        }
+      ]
+    })
+    .populate({
       path: 'likedPosts',
-      populate: ({
+      populate: {
         path: 'post',
-        model: 'Post'
-      })
-    }).populate({
+        populate: [
+          {
+            path: 'author',
+            model: 'User'
+          },
+          {
+            path: 'likes',
+            populate: {
+              path: 'author',
+              model: 'User'
+            }
+          }
+        ]
+      }
+    })
+    .populate({
       path: 'followers',
-      populate: ({
+      populate: {
         path: 'user',
         model: 'User'
-      })
-    }).populate({
+      }
+    })
+    .populate({
       path: 'following',
-      populate: ({
+      populate: {
         path: 'user',
         model: 'User'
-      })
+      }
     });
+    // .populate({
+    //   path: 'posts',
+    //   populate: ({
+    //     path: 'author',
+    //     model: 'User'
+    //   })
+    // }).populate({
+    //   path: 'likedPosts',
+    //   populate: ({
+    //     path: 'post',
+    //     model: 'Post'
+    //   })
+    // }).populate({
+    //   path: 'followers',
+    //   populate: ({
+    //     path: 'user',
+    //     model: 'User'
+    //   })
+    // }).populate({
+    //   path: 'following',
+    //   populate: ({
+    //     path: 'user',
+    //     model: 'User'
+    //   })
+    // });
 
     return NextResponse.json({
       success: true,
