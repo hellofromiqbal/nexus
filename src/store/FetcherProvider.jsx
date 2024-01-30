a0eda5640f2c3fde32dcf7ae4eb8bbe4e3cf1ab9
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-30 10:02:48 +0700

    set cache to no-store in fetcher provider fetch

--- src/store/FetcherProvider.jsx
+++ src/store/FetcherProvider.jsx
@@ -10,14 +10,14 @@ export const FetcherProvider = ({ children }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch('/api/users/me')
+    fetch('/api/users/me', { cache: 'no-store' })
       .then((res) => res.json())
       .then((data) => dispatch(setCurrentUser(data.data)))
       .catch((err) => console.log(err.message));
   }, []);
 
   useEffect(() => {
-    fetch('/api/posts/')
+    fetch('/api/posts/', { cache: 'no-store' })
       .then((res) => res.json())
       .then((data) => dispatch(setCurrentPosts(data.data)))
       .catch((err) => console.log(err.message));
